e7a0d92e37072692a88225963c81b802c553c9f8
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-12-31 17:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function insert_Row() {
 
   // Create the left cell (td) for the new row
   const cell1 = newRow.insertCell(0);  // Insert at position 0 (left cell)
-  cell1.textContent = "New Cell1";  // Set the text content to "New Cell1"
+  cell1.innerText = "New Cell1";  // Set the text content to "New Cell1"
 
   // Create the right cell (td) for the new row
   const cell2 = newRow.insertCell(1);  // Insert at position 1 (right cell)
